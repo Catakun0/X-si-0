961d65204c30002d7f709ef4da1274643f2e4d79
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-07 09:32:05 +0300

    Update lab01.hpp

--- lab01.hpp
+++ lab01.hpp
@@ -2,13 +2,26 @@
 class Board{
     int sizeBoard[3][3];
     int setMove(int x, int y, bool Xmove);
+    void check_If_is_empty(int x, int y);
     bool checkWin();
 };
 class Player{
+    int maxPlayers = 2;
     char name[20];
     bool isX;
 };
 class Position{
     int x;
     int y;
+};
+class Painter{
+    void Draw();
+};
+class Engine{
+    void StartGame();
+    void RestartGame();
+    void EndGame();
+};
+class RandomPlay{
+    bool playerStart;
 };
