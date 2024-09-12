6283f000dde79b5825a4da48edfb0126707e6cbb
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-12 23:17:37 +0300

    Update Player.hpp

--- X si 0/hpp/Player.hpp
+++ X si 0/hpp/Player.hpp
@@ -2,7 +2,7 @@
 
 class Player{ 
 public:
-    int maxPlayers = 2; //Jucatori maxim intro partida
+    static int maxPlayers; //Jucatori maxim intro partida
     char name[20]; //Numele jucatorilor cu maxim 20 de caractere
     bool isX; //Jucatorul este "X" sau "0"
 };
