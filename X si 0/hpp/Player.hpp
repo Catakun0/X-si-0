8adecbde597e68ea82b9fe7f598047249d3fd189
Tăbîică Cătălin <146533649+Catakun0@users.noreply.github.com>
2024-09-12 23:11:28 +0300

    Delete X si 0/hpp directory

--- X si 0/hpp/Player.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Player{ 
-public:
-    int maxPlayers = 2; //Jucatori maxim intro partida
-    char name[20]; //Numele jucatorilor cu maxim 20 de caractere
-    bool isX; //Jucatorul este "X" sau "0"
-};
