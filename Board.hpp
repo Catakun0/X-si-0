089ad276102bbe2f811059a0abc42ec25847cf1e
Tăbîică Cătălin <146533649+Catakun0@users.noreply.github.com>
2024-09-12 19:08:11 +0300

    Delete Board.hpp

--- Board.hpp
+++
@@ -1,8 +0,0 @@
-
-class Board{ //program one
-    char squere[9]; //sizeBoard [3][3]
-    public:
-    int setMove(int position, bool Xmove);
-    void check_If_is_empty(int position);
-    bool checkWin();
-};
