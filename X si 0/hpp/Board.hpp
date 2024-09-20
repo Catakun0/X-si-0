165b8438eddd80a38ab69e03d5b827e76894e762
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-20 11:47:07 +0300

    Delete Board.hpp

--- X si 0/hpp/Board.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Board{ 
-    char squere[9]; //sizeBoard [3][3]
-    public:
-    int setMove(int position, bool Xmove); //positioneaza "X" sau "0"
-    bool check_If_is_empty(int position); //verifica daca este gol patratul pentru inserare
-    bool checkWin();  //verifica daca exista un castigator
-};
