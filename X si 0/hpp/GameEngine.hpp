98700580eda7481f051535644cbff9d07617687d
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-20 11:47:23 +0300

    Delete GameEngine.hpp

--- X si 0/hpp/GameEngine.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Engine{
-    public:  //Functinalitatea jocului
-    void StartGame(); //Porneste jocul
-    void RestartGame(); //Reporneste o noua partida
-    void EndGame(); //Inchide jocul
-}; 
