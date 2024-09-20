3de51dd6c4e936dba653df4b3789d19eea1fe7cc
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-20 13:56:29 +0300

    Create GameEngine.hpp

---
+++ X si 0_02/GameEngine.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+class GameEngine{
+    public:  //Functinalitatea jocului
+    void StartGame(); //Porneste jocul
+    void RestartGame(); //Reporneste o noua partida
+    void EndGame(); //Inchide jocul
+}; 
