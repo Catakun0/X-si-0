edb872fd69a3b0789decf7d0acf1746b62f03e47
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-20 11:47:20 +0300

    Delete Player.hpp

--- X si 0/hpp/Player.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Player{ 
-public:
-    static int maxPlayers; //Jucatori maxim intro partida
-    char name[20]; //Numele jucatorilor cu maxim 20 de caractere
-    bool isX; //Jucatorul este "X" sau "0"
-};
