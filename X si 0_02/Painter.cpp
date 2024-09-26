28ec8c78aa62a004a3ab497d7dd74667d2573e0b
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-26 16:51:46 +0300

    Update Painter.cpp

--- X si 0_02/Painter.cpp
+++ X si 0_02/Painter.cpp
@@ -1,5 +1,10 @@
-#include "Painter.hpp"
+﻿#include "Painter.hpp"
 
 void Painter::Draw() {
 
+    char message[] = "Apasati orice tasta pentru a incepe jocul...\n";
+
+    for (int i = 0; message[i] != '\0'; ++i) {
+        putchar(message[i]);
+    }
 }
