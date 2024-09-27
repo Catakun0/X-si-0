f3c6daa990b293b61711e55880df2c62c1335a02
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 10:22:37 +0300

    Update Player.cpp

--- X si 0_02/Player.cpp
+++ X si 0_02/Player.cpp
@@ -1,2 +1,15 @@
-#include "Player.hpp"
+﻿#include "Player.hpp"
+#include "Painter.hpp"
+#include <iostream>
+#include <string> 
 
+int Player::maxPlayers = 2;
+
+void Player::playerName() { // Introducerea numelui
+
+    std::cout << "Introduceti numele first_Player: ";
+    std::getline(std::cin, Painter::player1Name); 
+
+    std::cout << "Introduceti numele second_Player: ";
+    std::getline(std::cin, Painter::player2Name); 
+}
