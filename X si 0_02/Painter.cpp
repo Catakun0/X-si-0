6b4170cc619403df8459382ad4ded6ec8e162c5d
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 10:22:40 +0300

    Update Painter.cpp

--- X si 0_02/Painter.cpp
+++ X si 0_02/Painter.cpp
@@ -1,5 +1,24 @@
 ﻿#include "Painter.hpp"
+#include "Player.hpp"
+#include <iostream> 
+
+std::string Painter::player1Name = "";
+std::string Painter::player2Name = "";
 
 void Painter::Draw(int i) {
+	if (i == 1) {
+		std::cout << "Sa inceapa jocul \n";
 
+		return;
+	}
+	else if (i == 2) {
+		std::cout << "Numele jucătorilor: " << player1Name << " si " << player2Name << std::endl;
+
+		return;
+	}
+	else if (i == 10){
+		std::cout << "[1] ""[2] ""[3] \n[4] ""[5] ""[6] \n[7] ""[8] ""[9] \n";
+
+		return;
+	}
 }
