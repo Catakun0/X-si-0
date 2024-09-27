679210259d68ebc9271472eb9d4c22354db1db4b
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 17:50:12 +0300

    Update RandomPlay.hpp

--- X si 0_02/RandomPlay.hpp
+++ X si 0_02/RandomPlay.hpp
@@ -2,5 +2,6 @@
 
 class RandomPlay{
     public: //RandomPlayerStart
-    int playerStart; //Va face prima decizia un jucator la intamplarea, "player1" sau "player2" va incepe
+    static int playerStart; //Va face prima decizia un jucator la intamplarea, "player1" sau "player2" va incepe
+    void randomGenerate();
 };
