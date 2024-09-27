46a028453174f53695d5aaca15b43fe59fd0dbf4
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 17:50:13 +0300

    Update RandomPlay.cpp

--- X si 0_02/RandomPlay.cpp
+++ X si 0_02/RandomPlay.cpp
@@ -1,2 +1,16 @@
-#include "RandomPlay.hpp"
+﻿#include "RandomPlay.hpp"
+#include <iostream>
+#include <random>
+#include <chrono>
 
+int RandomPlay::playerStart = 0;
+
+void RandomPlay::randomGenerate() {
+    //ramdomizare bazata pe timp
+    std::random_device rd;
+    std::mt19937 gen(std::chrono::system_clock::now().time_since_epoch().count());
+
+    std::uniform_int_distribution<> distr(1, 2);
+    RandomPlay::playerStart = distr(gen);
+    //std::cout << RandomPlay::playerStart << std::endl;
+}
