cd3f02f94d6d71776df1778e1dc8b9c8970e00cd
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 10:22:30 +0300

    Update Player.hpp

--- X si 0_02/Player.hpp
+++ X si 0_02/Player.hpp
@@ -1,8 +1,8 @@
 #pragma once
+#include <string>
 
 class Player{ 
 public:
     static int maxPlayers; //Jucatori maxim intro partida
-    char name[20]; //Numele jucatorilor cu maxim 20 de caractere
-    bool isX; //Jucatorul este "X" sau "0"
+    void playerName(); //introducere numelor hpp
 };
