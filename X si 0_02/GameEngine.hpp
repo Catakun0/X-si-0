c088e231c1e2a4779ef2ed4f4aae1f9b67c05ca1
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 19:54:57 +0300

    Update GameEngine.hpp

--- X si 0_02/GameEngine.hpp
+++ X si 0_02/GameEngine.hpp
@@ -3,6 +3,4 @@
 class GameEngine{
     public:  //Functinalitatea jocului
     void StartGame(); //Porneste jocul
-    void RestartGame(); //Reporneste o noua partida
-    void EndGame(); //Inchide jocul
 }; 
