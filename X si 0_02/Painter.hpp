9a6139b1157001f88f8ef8388101643e6b00d55d
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 19:54:53 +0300

    Update Painter.hpp

--- X si 0_02/Painter.hpp
+++ X si 0_02/Painter.hpp
@@ -1,10 +1,7 @@
 #pragma once
-#include <string>
+#include <iostream>
 
 class Painter{ 
     public: 
-    static std::string player1Name; // firstPlayer
-    static std::string player2Name; // secondPlayer
-
     void Draw(int i); //Afisarea datelor necesare inclusiv a tablei
 }; 
