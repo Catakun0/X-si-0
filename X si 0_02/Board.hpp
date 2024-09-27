ea9ed2e8b20f2271f4e8502e09be5c0b385962a8
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 11:08:41 +0300

    Update Board.hpp

--- X si 0_02/Board.hpp
+++ X si 0_02/Board.hpp
@@ -1,8 +1,9 @@
 #pragma once
+#include <iostream> 
 
 class Board{ 
-    char squere[9]; //sizeBoard [3][3]
     public:
+    static std::string squere; //sizeBoard [3][3]
     int setMove(int position, bool Xmove); //positioneaza "X" sau "0"
     bool check_If_is_empty(int position); //verifica daca este gol patratul pentru inserare
     bool checkWin();  //verifica daca exista un castigator
