619ae2b7766150410f56d2fe42463c74c1553003
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-27 23:42:06 +0300

    Update make.bat

--- X si 0_02/make.bat
+++ X si 0_02/make.bat
@@ -5,6 +5,6 @@ g++ Painter.cpp -o Painter.o -c
 g++ Player.cpp -o Player.o -c
 g++ RandomPlay.cpp -o RandomPlay.o -c
 g++ main.cpp -o main.o -c
-g++ Board.o GameEngine.o Painter.o Player.o RandomPlay.o main.o -o game.exe
+g++ Board.o GameEngine.o Painter.o Player.o RandomPlay.o main.o -o tic_tac_toe.exe
 
 
