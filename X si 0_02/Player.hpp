1f69a1e1360396d2f6924c86390d6bdc426c0329
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-28 09:11:33 +0300

    Update Player.hpp

--- X si 0_02/Player.hpp
+++ X si 0_02/Player.hpp
@@ -6,4 +6,5 @@ public:
     static int maxPlayers; //Jucatori maxim intro partida
     void playerName(); //introducere numelor hpp
     static std::string firstPlayer, secondPlayer;
+    static bool inseredNames;
 };
