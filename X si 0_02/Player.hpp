21504bff02e2fbf99264e29165dec2aac0f86dea
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-28 12:47:01 +0300

    Update Player.hpp

--- X si 0_02/Player.hpp
+++ X si 0_02/Player.hpp
@@ -4,7 +4,7 @@
 class Player{ 
 public:
     static int maxPlayers; //Jucatori maxim intro partida
-    void playerName(); //introducere numelor hpp
-    static std::string firstPlayer, secondPlayer;
-    static bool inseredNames;
+    void playerName(); //introducere numelor 
+    static std::string firstPlayer, secondPlayer; //variabelele pentru numele jucatorilor
+    static bool inseredNames; //un bool care va fi true in cazul in care am introdus o data numele jucatorilor
 };
