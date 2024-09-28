4af2cd20109619c9d02977ceda4df481bdbb6300
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-28 09:11:32 +0300

    Update RandomPlay.cpp

--- X si 0_02/RandomPlay.cpp
+++ X si 0_02/RandomPlay.cpp
@@ -1,8 +1,12 @@
 ﻿#include "RandomPlay.hpp"
 #include <iostream>
+#include <ctime>
 
 int RandomPlay::playerStart = 0;
 
-void RandomPlay::randomGenerate() {
-    playerStart = 1;
+void RandomPlay::randomGenerate() { //o randomizare bazata pe timp (nu-mi lucra cu <random>)
+
+    srand(time(0));
+    playerStart = rand() % 2 + 1;
+
 }
