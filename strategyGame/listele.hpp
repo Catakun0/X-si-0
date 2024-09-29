24317eaac7c9db44019cf694445adbd2aaf89b9b
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-29 19:36:27 +0300

    Update listele.hpp

--- strategyGame/listele.hpp
+++ strategyGame/listele.hpp
@@ -1,7 +1,11 @@
 #pragma once
-#include <iostrem>
-#include <vecotr>
+#include <iostream>
+#include <vector>
+#include "Enemy.hpp"
+#include "Unitati.hpp"
 
 class listele{
-
+    public:
+    std::vector<Enemy> enemyList;
+    std::vector<Unitati> unityList;
 };
