422d24a0a03e88471917c57d633b10eee8995451
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-29 18:57:58 +0300

    Create GameEngine.cpp

---
+++ strategyGame/GameEngine.cpp
@@ -0,0 +1,15 @@
+#include "GameEngine.hpp"
+#include "createEnemy.hpp"
+#include "createUnity.hpp"
+#include <iostream>
+
+void GameEngine::startGame() {
+	createEnemy createEnemy_now;
+	createUnity createUnity_now;
+	createEnemy_now.generareEnemy(2);
+
+	do 
+	{
+
+	} while (true);
+}
