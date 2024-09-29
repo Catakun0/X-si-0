597c8d0a9c31451ad76073d1d5ba31b4dc6ca378
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-29 20:44:07 +0300

    Update createUnity.cpp

--- strategyGame/createUnity.cpp
+++ strategyGame/createUnity.cpp
@@ -1,5 +1,22 @@
 #include "createUnity.hpp"
+#include "Unitati.hpp"
+#include "listele.hpp"
+#include <iostream>
 
 void createUnity::generareUnity(int count) {
-
+	int i = 0;
+	do 
+	{
+		Unitati unitati;
+		unitati.unityName = "MyUnity " + std::to_string(i);
+		unitati.unityHP = 100;
+		unitati.unityPower = 40;
+		unitati.unityDefence = 200;
+		listele::unityList.emplace_back(unitati);
+		i++;
+		if (i > count) 
+		{
+			return;
+		}
+	} while (true);
 }
