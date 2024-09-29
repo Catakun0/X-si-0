3a90d27f6d433a4b685a5e946df9b4517f3e7669
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-29 20:44:00 +0300

    Update GameEngine.cpp

--- strategyGame/GameEngine.cpp
+++ strategyGame/GameEngine.cpp
@@ -1,12 +1,16 @@
 #include "GameEngine.hpp"
 #include "createEnemy.hpp"
 #include "createUnity.hpp"
+#include "Paint.hpp"
 #include <iostream>
 
 void GameEngine::startRound() {
 	createEnemy createEnemy_now;
 	createUnity createUnity_now;
-	createEnemy_now.generareEnemy(2);
+	Paint paint;
+	createEnemy_now.generareEnemy(5);
+	createUnity_now.generareUnity(3);
+	paint.afisare(0);
 
 	do 
 	{
