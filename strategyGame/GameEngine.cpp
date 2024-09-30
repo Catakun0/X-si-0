29f8cfc900e63740dcc5c52ed5a79008e74e0612
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-30 18:44:42 +0300

    Update GameEngine.cpp

--- strategyGame/GameEngine.cpp
+++ strategyGame/GameEngine.cpp
@@ -2,14 +2,17 @@
 #include "createEnemy.hpp"
 #include "createUnity.hpp"
 #include "Paint.hpp"
+#include "mainAccount.hpp"
 #include <iostream>
 
 void GameEngine::startRound() {
 	createEnemy createEnemy_now;
 	createUnity createUnity_now;
 	Paint paint;
-	createEnemy_now.generareEnemy(5);
-	createUnity_now.generareUnity(3);
+	mainAccount mainAccount_now;
+	//start the first game
+	mainAccount_now.managementAccount();
+	createUnity_now.generareUnity();
 	paint.afisare(0);
 
 	do 
