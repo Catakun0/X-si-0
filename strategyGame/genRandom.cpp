d15870ed1baf3468fcaf8addbaa701a12672bd02
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-30 18:44:41 +0300

    Create genRandom.cpp

---
+++ strategyGame/genRandom.cpp
@@ -0,0 +1,28 @@
+﻿#include "genRandom.hpp"
+#include "mainAccount.hpp"
+#include <windows.h>  
+
+int genRandom::random(int min, int max) {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<> distrib(min, max);
+    int randomNow = distrib(gen);
+
+    if (mainAccount::levelAccount <= 10) 
+    {
+        Sleep(600);
+    }
+    else if (mainAccount::levelAccount <= 20)
+    {
+        Sleep(400);
+    }
+    else if (mainAccount::levelAccount <= 30)
+    {
+        Sleep(200);
+    }
+    else 
+    {
+        Sleep(100);
+    }
+    return randomNow;
+}
