e91e23a48789c482731fdc63ecd803ab8cf57296
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-30 18:44:35 +0300

    Update Paint.cpp

--- strategyGame/Paint.cpp
+++ strategyGame/Paint.cpp
@@ -1,31 +1,69 @@
 ﻿#include "Paint.hpp"
 #include "iostream"
 #include "listele.hpp"
+#include <cstdlib>
+#include <windows.h>
 
 void Paint::afisare(int i) {
-    if (i == 0) {
-        int selector = 0;
-        std::cout << "\n1 - Afisarea unitatilor detinute" << std::endl;
-        std::cout << "2 - Generarea de unitati" << std::endl;
-        std::cout << "3 - Mergi la urmatoarea lupta" << std::endl;
-        std::cin >> selector;
-        switch (selector) {
-        case 1: {
-            if (!listele::unityList.empty()) {
-                std::cout << "Numarul de unitati pe care il detineti este de: " << listele::unityList.size() << " unitati" << std::endl;
-                for (int j = 0; j < listele::unityList.size(); j++) {
-                    std::cout << "Numele: " << listele::unityList[j].unityName << "/ HP: " << listele::unityList[j].unityHP << "/ Power: "
-                        << listele::unityList[j].unityPower << "/ Defence: " << listele::unityList[j].unityDefence << std::endl;
+    
+    do 
+    {
+        if (i == -1)
+        {
+            std::system("cls");
+            return;
+        }
+        if (i == 0) {
+            int selector = 0;
+            std::cout << "\n1 - Afisarea unitatilor detinute" << std::endl;
+            std::cout << "2 - Generarea de unitati" << std::endl;
+            std::cout << "3 - Mergi la urmatoarea lupta" << std::endl;
+            std::cin >> selector;
+            switch (selector) {
+            case 1: //afisarea unitatilor
+            {
+                if (!listele::magList.empty() || !listele::undeadList.empty() || !listele::kingList.empty())
+                {
+                    if (!listele::magList.empty())
+                    {
+                        std::cout << "Afisarea magilor... \n" << std::endl;
+                        Sleep(1000);
+                        std::system("cls");
+                        for (int j = 0; listele::magList.size() > j; j++) 
+                        {
+                            std::cout << "Name: " << listele::magList[j].name <<
+                                "HP: " << listele::magList[j].hp <<
+                                "Power: " << listele::magList[j].power <<
+                                "Magical Power: " << listele::magList[j].magicalPower <<
+                                "MP: " << listele::magList[j].mp <<
+                                "Defence: " << listele::magList[j].defence <<
+                                std::endl;
+                        }
+                    }
+                    else if (!listele::undeadList.empty())
+                    {
+
+                    }
+                    else if (!listele::kingList.empty())
+                    {
+
+                    }
                 }
+                else
+                {
+                    std::cout << "Nu aveti unitati detinute." << std::endl;
+                }
+                break;
             }
-            else {
-                std::cout << "Nu aveti unitati detinute." << std::endl;
+            default:
+                std::cout << "Alegeti o optiune valida! " << std::endl;
+                break;
             }
-            break;
         }
-        default:
-            break;
+        if (i == 1)
+        {
+            std::cout << "Generare..." << std::endl;
         }
-    }
-    return;
+
+    } while (true);
 }
