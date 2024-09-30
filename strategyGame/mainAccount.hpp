e4e30e813f3b3007bc3bd6ac5964f3c3acd31e6d
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-09-30 18:44:33 +0300

    Create mainAccount.hpp

---
+++ strategyGame/mainAccount.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+class mainAccount{
+    public:
+    static int levelAccount;
+    static int maxOwnedUnity;
+    static int unity;
+    static int points;
+
+    void managementAccount();
+};
