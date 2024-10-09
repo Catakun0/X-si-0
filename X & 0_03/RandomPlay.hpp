104d2e4fcb3beafc9c21a18999998ab5301fd4e3
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-09 19:01:30 +0300

    Create RandomPlay.hpp

---
+++ X & 0_03/RandomPlay.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class RandomPlay{
+    public: 
+    static int playerStart; //Va face prima decizia un jucator la intamplarea, "player1" sau "player2" va incepe in dependenta daca variabila pica 1 sau 2
+    void randomGenerate(); //metoda care o sa genereze un numar la intamplare
+};
+
+
