71cec5275f5d6f1de461d6862d8e747a42a68ef1
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-09 19:01:40 +0300

    Create GameEngine.cpp

---
+++ X & 0_03/GameEngine.cpp
@@ -0,0 +1,23 @@
+﻿#include "GameEngine.hpp"
+#include "Painter.hpp"
+#include "RandomPlay.hpp"
+#include "Board.hpp"
+#include "Player.hpp"
+#include <iostream>
+
+void GameEngine::StartGame() {
+	Painter painter; //instantele
+	Player player;
+	RandomPlay randomPlay;
+	Board board;
+
+	do 
+	{
+		board.inserareaSquer(); //inseram tabla de la 1-9
+		randomPlay.randomGenerate(); //incepe la intaplare primul sau al doilea player
+		player.playerName(); //introducem numele 
+		painter.Draw(1); //afiseaza jocul
+		painter.Draw(2); //meniul de final
+
+	} while (true);
+}
