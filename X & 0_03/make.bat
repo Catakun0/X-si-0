cad994a32b6345ca32a5ec364dfe3f3ec7d056d7
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-09 19:01:36 +0300

    Create make.bat

---
+++ X & 0_03/make.bat
@@ -0,0 +1,10 @@
+@echo off
+g++ Board.cpp -o Board.o -c
+g++ GameEngine.cpp -o GameEngine.o -c
+g++ Painter.cpp -o Painter.o -c
+g++ Player.cpp -o Player.o -c
+g++ RandomPlay.cpp -o RandomPlay.o -c
+g++ main.cpp -o main.o -c
+g++ Board.o GameEngine.o Painter.o Player.o RandomPlay.o main.o -o tic_tac_toe.exe
+
+
