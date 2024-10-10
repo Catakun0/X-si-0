421db9b85ff993a499434d97aedc1ea2a554ab6f
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-10 12:33:19 +0300

    Update Painter.hpp

--- X & 0_03/Painter.hpp
+++ X & 0_03/Painter.hpp
@@ -3,5 +3,12 @@
 
 class Painter{ 
     public: 
+
+    int select1;
+
     void Draw(int i); //Afisarea datelor necesare inclusiv a tablei
+
+    friend std::istream& operator>>(std::istream& in, Painter& painter);
+    friend std::ostream& operator<<(std::ostream& out, const Painter& painter);
+
 }; 
