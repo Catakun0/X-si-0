550faeaf025257b8f7e39d7ca21293716aa5068d
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-11 17:28:53 +0300

    Update Painter.hpp

--- X & 0_03/Painter.hpp
+++ X & 0_03/Painter.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include <iostream>
+#include "Board.hpp"
 
 class Painter{ 
     public: 
 
     int select1;
+    Board* board;
+
+    Painter(Board* b) : board(b) {}
 
     void Draw(int i); //Afisarea datelor necesare inclusiv a tablei
 
