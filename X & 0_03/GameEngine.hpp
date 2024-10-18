11bff9a5a4a6d6bc90df5cbbe43d8c2300222ce8
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-18 11:10:48 +0300

    Update GameEngine.hpp

--- X & 0_03/GameEngine.hpp
+++ X & 0_03/GameEngine.hpp
@@ -1,6 +1,16 @@
 #pragma once
+#include <iostream>
 
 class GameEngine{
-    public:  //Functinalitatea jocului
+    public:
+    GameEngine(); //implicit
+    GameEngine(const GameEngine& other); // de copiere
+    GameEngine(const bool startGame); // cu parametru
+
+    GameEngine& operator = (const GameEngine& other);//operator de copiere
+    bool operator == (const GameEngine& other) const;
+    friend std::ostream& operator << (std::ostream& os, const GameEngine& gameEngine);
+    friend std::istream& operator >> (std::istream& is, GameEngine& gameEngine);
+
     void StartGame(); //Porneste jocul
 }; 
