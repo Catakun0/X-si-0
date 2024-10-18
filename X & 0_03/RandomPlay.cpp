d5f7478cae494af9510f65d7025c6af2f43e32f5
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-18 18:31:31 +0300

    Update RandomPlay.cpp

--- X & 0_03/RandomPlay.cpp
+++ X & 0_03/RandomPlay.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <ctime>
 
+int RandomPlay::playerStart = 0;
+
 // Constructor implicit
 RandomPlay::RandomPlay()
 {
@@ -20,30 +22,38 @@ RandomPlay::RandomPlay(const int initialWhoStart)
 }
 
 //Operator de copiere
-RandomPlay& RandomPlay::operator=(const RandomPlay& other)
+RandomPlay& RandomPlay::operator = (const RandomPlay& other)
 {
-    
+    if (this != &other) 
+    {
+
+    }
+    return *this;
 }
 
 //Operator de comparatie
-bool RandomPlay::operator==(const RandomPlay& other) const {
-    
+bool RandomPlay::operator == (const RandomPlay& other) const 
+{
+    return true; 
 }
 
-// Operator de afisare
-std::ostream& operator<<(std::ostream& os, const RandomPlay& randomPlay) {
-    
+//Operator de afisare
+std::ostream& operator<<(std::ostream& os, const RandomPlay& randomPlay)
+{
+    os << "Player -> " << randomPlay.playerStart << " Incepe" << std::endl;
+    return os;
 }
 
-// Operator de citire
-std::istream& operator>>(std::istream& is, RandomPlay& randomPlay) {
-    
+//Operator de citire
+std::istream& operator>>(std::istream& is, RandomPlay& randomPlay) 
+{
+    return is;
 }
-
-int RandomPlay::playerStart = 0;
 
 void RandomPlay::randomGenerate() { //o randomizare bazata pe timp (nu-mi lucra cu <random>)
 
     srand(time(0));
     playerStart = rand() % 2 + 1;
+    std::cout << *this;
+    return;
 }
