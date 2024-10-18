2405beb56ec4b3ddafef8be4380db015dc0a5e63
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-18 11:10:52 +0300

    Update Board.hpp

--- X & 0_03/Board.hpp
+++ X & 0_03/Board.hpp
@@ -8,7 +8,14 @@ class Board{
 
     Board(); //Constructor mplicit
     Board (const Board& other); //constructor de copiere
+    Board(const int initialHightBoard); // construcotr cu parametru
 
+    Board& operator = (const Board& other);//operator de copiere
+    bool operator == (const Board& other) const; // operator de comparatie
+    friend std::ostream& operator << (std::ostream& os, const Board& Board); // operator de afisare
+    friend std::istream& operator >> (std::istream& is, Board& Board); // operator de citire
+
+    void initializareaTablei();
     bool check_If_is_empty(int position); //verifica daca este gol patratul pentru inserare
     int checkWin();  //verifica daca exista un castigator
 };
