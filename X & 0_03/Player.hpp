3f983a0779a2bb9659af7eb6e2898cfddffb4ca5
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-18 11:10:43 +0300

    Update Player.hpp

--- X & 0_03/Player.hpp
+++ X & 0_03/Player.hpp
@@ -9,6 +9,11 @@ public:
     static bool inseredNames; //un bool care va fi true in cazul in care am introdus o data numele jucatorilor
 
     Player(); //constructor implicit
-    Player(const Player& other); //operatorii de copiere
-    bool operator==(const Player& other) const; // de comparatie
+    Player(const Player& other); //constructor de copiere
+    Player(const int initialMaxPlayers); // construcotr cu parametru
+
+    Player& operator = (const Player& other); //operator de copiere
+    bool operator==(const Player& other) const; //operator de comparatie
+    friend std::ostream& operator << (std::ostream& os, const Player& player); // operator de afisare
+    friend std::istream& operator >> (std::istream& is, Player& player); // operator de citire
 };
