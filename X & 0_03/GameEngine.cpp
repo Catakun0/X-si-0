6d37826e5db2ddd8de7fe8b5ffe477c7a7e1d711
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-18 11:10:51 +0300

    Update GameEngine.cpp

--- X & 0_03/GameEngine.cpp
+++ X & 0_03/GameEngine.cpp
@@ -5,17 +5,41 @@
 #include "Player.hpp"
 #include <iostream>
 
+GameEngine::GameEngine()
+{
+
+}
+
+GameEngine::GameEngine(const GameEngine& other)
+{
+
+}
+GameEngine::GameEngine(const bool startGame) {
+
+}
+GameEngine& GameEngine::operator = (const GameEngine& other) {
+
+}
+bool GameEngine::operator == (const GameEngine& other) const {
+
+}
+std::ostream& operator << (std::ostream os, const GameEngine& gameEngine) {
+
+}
+std::istream& operator >> (std::istream is, GameEngine& gameEngine) {
+
+}
+
 void GameEngine::StartGame() {
 	//instantele
 	Player player;
 	RandomPlay randomPlay;
-	Board boardOriginal; //creem un obect origianl
-	Board boardCopy(boardOriginal); //si aici fiind in "start" obectul, boardCopy va fi egal cu boardOriginal, apeland constructorul de copiere
-	Painter painter(&boardCopy);
+	Board boardOriginal;
+	Painter painter(&boardOriginal);
 
 	do 
 	{
-		boardCopy = boardOriginal; //aici deja facem o copiere superficiala
+		boardOriginal.initializareaTablei();
 		randomPlay.randomGenerate(); //incepe la intaplare primul sau al doilea player
 		player.playerName(); //introducem numele 
 		painter.Draw(1); //afiseaza jocul
