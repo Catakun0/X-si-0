6adad3836a788990f5c6f85bbcac59bd648f3dc9
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-10-18 11:10:42 +0300

    Update RandomPlay.cpp

--- X & 0_03/RandomPlay.cpp
+++ X & 0_03/RandomPlay.cpp
@@ -2,6 +2,31 @@
 #include <iostream>
 #include <ctime>
 
+RandomPlay::RandomPlay()
+{
+
+}
+
+RandomPlay::RandomPlay(const RandomPlay& other)
+{
+
+}
+RandomPlay::RandomPlay(const int initialWhoStart) {
+
+}
+RandomPlay& RandomPlay::operator = (const RandomPlay& other) {
+
+}
+bool RandomPlay::operator == (const RandomPlay& other) const {
+
+}
+std::ostream& operator << (std::ostream os, const RandomPlay& randomPlay) {
+
+}
+std::istream& operator >> (std::istream is, RandomPlay& brandomPlay) {
+
+}
+
 int RandomPlay::playerStart = 0;
 
 void RandomPlay::randomGenerate() { //o randomizare bazata pe timp (nu-mi lucra cu <random>)
