5bb6c3edf0b0b4739b9419a5349508747f79a670
Tăbîică Cătălin <tabiicacatalin@gmail.com>
2024-12-05 12:05:23 +0200

    Update Board.hpp

--- Lab.06/Board.hpp
+++ Lab.06/Board.hpp
@@ -1,20 +1,37 @@
 #pragma once
 #include <iostream> 
 #include <vector>
-
+ 
 class Board{ 
     public:
     
     std::vector<std::string> squere;
+    /// @brief Constructorul implicit Board folosit pentru inserarea table de la 1-9
+    Board(); //Constructor mplicit 
 
-    Board(); //Constructor mplicit
-    Board (const Board& other); //constructor de copiere
-    Board(const int initialHightBoard); // construcotr cu parametru
+    /// @brief Folosirea Constructorului de copiere pentru a copia un alt board
+    /// @param other reprezentand o tabla diferita
+    Board (const Board& other);
 
-    Board& operator = (const Board& other);//operator de copiere
-    bool operator == (int position) const; // operator de comparatie
-    friend std::ostream& operator << (std::ostream& os, const Board& Board); // operator de afisare
-    friend std::istream& operator >> (std::istream& is, Board& Board); // operator de citire
+    /// @brief Constructorul cu parametru
+    /// @param initialHightBoard variabila fiind folosita pentru difinirea marimei tablei 
+    Board(const int initialHightBoard);
+
+    /// @brief Operatorul de copiere
+    /// @param other reprezetand o alta tabla
+    Board& operator = (const Board& other);
+
+    /// @brief Operatorul de comparare folosit pentru a detecta daca pe pozitia data
+    /// @param position care o primeste ca prametru, exista un "0" sau "X"
+    /// @return si returneaza true daca nu exista si false daca exista
+    bool operator == (int position) const; 
+
+    /// @brief 
+    /// @param os 
+    /// @param Board 
+    /// @return 
+    friend std::ostream& operator << (std::ostream& os, const Board& Board); 
+    friend std::istream& operator >> (std::istream& is, Board& Board);
 
     void initializareaTablei();
     bool check_If_is_empty(int position); //verifica daca este gol patratul pentru inserare
